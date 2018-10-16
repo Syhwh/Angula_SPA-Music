4e3ddedaa48586eae19e31de06599a5256b4a0fb
Shymmy W. Garcia <shymmywgarcia@gmail.com>
2018-10-16 10:43:38 -0500

    Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/shared/home/home.component';
+import { AboutComponent } from './components/shared/about/about.component';
+import { MusiciansComponent } from './components/shared/musicians/musicians.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    MusiciansComponent
   ],
   imports: [
     BrowserModule
